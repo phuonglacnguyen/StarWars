8cb3f5c488fe2f587a9c7522b19268e136fb7ff1
phuong nguyen <phuong@xs4all.nl>
2024-03-11 20:46:13 +0100

    Host commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: { host: '10.0.0.128', port: 8000 },
 })
