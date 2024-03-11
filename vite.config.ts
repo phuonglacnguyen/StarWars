31d2d2228af718a29ad74797abe300c937f92249
phuong nguyen <phuong@xs4all.nl>
2024-03-11 19:53:28 +0100

    Fix install correcting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { host: '10.0.0.128', port: 8000 },
+  plugins: [react()]
 })
